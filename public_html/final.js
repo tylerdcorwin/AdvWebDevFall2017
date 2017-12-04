0c1bf983d7eab1e1ced0d5d6f8164af1b82a10c5
Tyler <tydcorwin@gmail.com>
2017-12-04 18:46:39 -0500

    week 8 code with notes on final

---
+++ public_html/final.js
@@ -0,0 +1,44 @@
+This is what is on the final:
+
+Most questions are going to about node.js
+  few will be about javascript
+
+These are the types of questions that may be asked:
+  -What is Node.JS?
+    Command line javascript
+    Its a PLATFORM!
+    express is a webform that runs on node.js
+    you can run multiple things with it.
+  -If you wanted to run a server on node.js how would you do it?
+    look at WEEK1 code(review it)
+  -Does node.js have a built in way to run a server
+    YES it has HTTP
+  -What language do you use for node.js
+    javascript
+  -What does db stand for in mongodb?
+    document! base
+  -What types of documents does mongo store?
+    Binary JSON!
+  -if One transaction fails in a regular database they will all fail, but
+  MONGODB is not a transitional database so if one fails others will still run
+  -what is the node.js that is not packaged with it?
+    express
+  -What do we use to install packages and plugins for node?
+    npm
+  -What does NPM stand for?
+    Node Package Manager
+
+
+  ACTUAL CODE PORTION WILL BE: (can be done in pairs)
+  -Using Express (know how to start an express server [scaffolding])
+  -creating a form on a page to collect data
+  -create a schema in order to store that information to a database
+  -connecting express to mongodb
+  -Basic validation for saving to a database
+  -using PUG!!!!
+    -can use express generator
+  -Know how to create a middleware
+    -must be able to:
+      -do something that will runin the application
+      -look at week 6 with the core examples
+  -create a view page that displays the content that we are saving as a json webpage (display in json)
